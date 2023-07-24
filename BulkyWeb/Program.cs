f155e47388ab8b62acbf64fd5b5c0adc99fde507
tgztk <96948081+tgztk@users.noreply.github.com>
2023-07-25 00:12:39 +0300

    Implementing Unif of Work

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,13 +1,17 @@
 using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.Contracts;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
