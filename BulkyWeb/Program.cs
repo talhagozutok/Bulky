3b1e723b4a8d1606498318ff07cea3ec4cbef53a
tgztk <96948081+tgztk@users.noreply.github.com>
2023-07-24 11:40:44 +0300

    Fixer navbar and footer view.

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -4,8 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews()
-    .AddRazorRuntimeCompilation(); // https://stackoverflow.com/a/70624531
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
