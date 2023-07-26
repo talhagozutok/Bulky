b08ab2433330cf98a9996406d0ea88a22c49483b
talhagozutok <talhagozutok@users.noreply.github.com>
2023-07-26 16:07:31 +0300

    Created ProductViewModel

---
+++ Bulky.Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Bulky.Models.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bulky.Models.ViewModels;
+public class ProductViewModel
+{
+	public Product Product { get; set; }
+
+	[ValidateNever]
+	public IEnumerable<SelectListItem> CategoryList { get; set; }
+}
