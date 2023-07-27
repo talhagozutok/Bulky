be4240a58668fc895742e9baecf3116658e56ba2
tgztk <96948081+tgztk@users.noreply.github.com>
2023-07-27 23:41:08 +0300

    Added more seed data

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -47,6 +47,78 @@ public class ApplicationDbContext : DbContext
 				PriceHundredOrMore = 70,
 				CategoryId = 2,
 				ImageUrl = ""
+			},
+			new Product
+			{
+				Id = 3,
+				Title = "Fortune of Time",
+				Author = "Billy Spark",
+				ISBN = "SWD9999001",
+				Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+				ListPrice = 99,
+				PriceFifty = 85,
+				PriceHundredOrMore = 80,
+				CategoryId = 2
+			},
+			new Product
+			{
+				Id = 4,
+				Title = "Dark Skies",
+				Author = "Nancy Hoover",
+				Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+				ISBN = "CAW777777701",
+				ListPrice = 40,
+				PriceFifty = 25,
+				PriceHundredOrMore = 20,
+				CategoryId = 2
+			},
+			new Product
+			{
+				Id = 5,
+				Title = "Vanish in the Sunset",
+				Author = "Julian Button",
+				Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+				ISBN = "RITO5555501",
+				ListPrice = 55,
+				PriceFifty = 40,
+				PriceHundredOrMore = 35,
+				CategoryId = 2
+			},
+			new Product
+			{
+				Id = 6,
+				Title = "Cotton Candy",
+				Author = "Abby Muscles",
+				Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+				ISBN = "WS3333333301",
+				ListPrice = 70,
+				PriceFifty = 60,
+				PriceHundredOrMore = 55,
+				CategoryId = 2
+			},
+			new Product
+			{
+				Id = 7,
+				Title = "Rock in the Ocean",
+				Author = "Ron Parker",
+				Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+				ISBN = "SOTJ1111111101",
+				ListPrice = 30,
+				PriceFifty = 25,
+				PriceHundredOrMore = 20,
+				CategoryId = 2
+			},
+			new Product
+			{
+				Id = 8,
+				Title = "Leaves and Wonders",
+				Author = "Laura Phantom",
+				Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+				ISBN = "FOT000000001",
+				ListPrice = 25,
+				PriceFifty = 22,
+				PriceHundredOrMore = 20,
+				CategoryId = 2
 			}
 		);
 	}
---
+++ Bulky.DataAccess/Migrations/20230727201405_SeedMoreData.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedMoreData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ISBN",
+                value: "3681a4b8-d9a1-43f4-b939-dc8f1aabf50b");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ISBN",
+                value: "46278f02-b8e4-4916-b59e-4c92c15cde04");
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "CategoryId", "Description", "ISBN", "ImageUrl", "ListPrice", "PriceFifty", "PriceHundredOrMore", "Title" },
+                values: new object[,]
+                {
+                    { 3, "Billy Spark", 2, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", "SWD9999001", null, 99.0, 85.0, 80.0, "Fortune of Time" },
+                    { 4, "Nancy Hoover", 2, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", "CAW777777701", null, 40.0, 25.0, 20.0, "Dark Skies" },
+                    { 5, "Julian Button", 2, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", "RITO5555501", null, 55.0, 40.0, 35.0, "Vanish in the Sunset" },
+                    { 6, "Abby Muscles", 2, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", "WS3333333301", null, 70.0, 60.0, 55.0, "Cotton Candy" },
+                    { 7, "Ron Parker", 2, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", "SOTJ1111111101", null, 30.0, 25.0, 20.0, "Rock in the Ocean" },
+                    { 8, "Laura Phantom", 2, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", "FOT000000001", null, 25.0, 22.0, 20.0, "Leaves and Wonders" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ISBN",
+                value: "989992e2-48d2-40cb-b685-3fc765b65c73");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ISBN",
+                value: "596db6de-73c8-48e7-8589-b61cf44cb629");
+        }
+    }
+}
