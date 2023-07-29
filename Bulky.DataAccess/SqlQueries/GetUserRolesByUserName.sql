69ac118b24546a6c808e770f299b5d7b572e77c7
tgztk <96948081+tgztk@users.noreply.github.com>
2023-07-30 00:08:12 +0300

    Use SQL query to quickly get user's role

---
+++ Bulky.DataAccess/SqlQueries/GetUserRolesByUserName.sql
@@ -0,0 +1,9 @@
+﻿USE BULKY;
+
+SELECT dbo.AspNetRoles.Id as UserId,
+	dbo.AspNetUsers.UserName as UserName,
+	dbo.AspNetRoles.Name as RoleName
+FROM dbo.AspNetUsers
+	JOIN dbo.AspNetUserRoles ON dbo.AspNetUserRoles.UserId = dbo.AspNetUsers.Id
+	JOIN dbo.AspNetRoles ON dbo.AspNetRoles.Id = dbo.AspNetUserRoles.RoleId
+WHERE UserName = 'admin@admin.com'
