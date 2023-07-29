047a4f985961e40ef305a891d61d8d2ffd7cf94b
tgztk <96948081+tgztk@users.noreply.github.com>
2023-07-29 21:07:12 +0300

    Updated StaticDetails

--- Bulky.Utilities/StaticDetails.cs
+++ Bulky.Utilities/StaticDetails.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bulky.Utilities;
+﻿namespace Bulky.Utilities;
 public static class StaticDetails
 {
-	public const string Role_Customer = "Customer";
-	public const string Role_Company = "Company";
-	public const string Role_Admin = "Admin";
-	public const string Role_Employee = "Employee";
+    public const string Role_Customer = "Customer";
+    public const string Role_Company = "Company";
+    public const string Role_Admin = "Admin";
+    public const string Role_Employee = "Employee";
+
+    public const string AdminEmail = "admin@admin.com";
+    public const string AdminPassword = "***REDACTED***";
 }
