5d3a2ca98fa8abae322c998f57c0c2cf5a600fce
talhagozutok <talhagozutok@users.noreply.github.com>
2023-07-29 21:15:43 +0300

    Redirection fixed for `Identity` pages

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -11,10 +11,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
 builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -27,9 +34,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -40,16 +47,16 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapAreaControllerRoute(
-		name: "Admin",
-		areaName: "Admin",
-		pattern: "{area=Admin}/{controller=Home}/{action=Index}/{id?}"
-	);
+        name: "Admin",
+        areaName: "Admin",
+        pattern: "{area=Admin}/{controller=Home}/{action=Index}/{id?}"
+    );
 
 app.MapAreaControllerRoute(
-		name: "Customer",
-		areaName: "Customer",
-		pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}"
-	);
+        name: "Customer",
+        areaName: "Customer",
+        pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}"
+    );
 
 app.MapRazorPages();
 
