02fd4725c9541f721ebffc6a71a92b10f6ff728f
talhagozutok <talhagozutok@users.noreply.github.com>
2023-07-31 15:59:04 +0300

    Added `ShoppingCart`

---
+++ Bulky.DataAccess/Repository/Contracts/IShoppingCartRepository.cs
@@ -0,0 +1,7 @@
+﻿using Bulky.Models.Entities;
+
+namespace Bulky.DataAccess.Repository.Contracts;
+public interface IShoppingCartRepository : IRepository<ShoppingCart>
+{
+    void Update(ShoppingCart cart);
+}
---
+++ Bulky.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,19 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.Contracts;
+using Bulky.Models.Entities;
+
+namespace Bulky.DataAccess.Repository;
+public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public ShoppingCartRepository(ApplicationDbContext dbContext) : base(dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Update(ShoppingCart cart)
+    {
+        _dbContext.ShoppingCarts.Update(cart);
+    }
+}
