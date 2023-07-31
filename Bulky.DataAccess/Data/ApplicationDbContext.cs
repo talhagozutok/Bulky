9bb2fe8d5f86d1f0bfbe33aa832978d46065a94f
tgztk <96948081+tgztk@users.noreply.github.com>
2023-07-31 15:59:04 +0300

    Added `ShoppingCart`

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -8,10 +8,9 @@ namespace Bulky.DataAccess.Data;
 public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
 	public DbSet<Category> Categories { get; set; }
-
 	public DbSet<Product> Products { get; set; }
 	public DbSet<Company> Companies { get; set; }
-
+	public DbSet<ShoppingCart> ShoppingCarts { get; set; }
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
 	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -9,21 +9,26 @@ public class UnitOfWork : IUnitOfWork
 	private ICategoryRepository Category { get; }
 	private IProductRepository Product { get; }
 	private ICompanyRepository Company { get; }
+	private IShoppingCartRepository ShoppingCart { get; }
 
     public UnitOfWork(ApplicationDbContext dbContext,
         ICategoryRepository categoryRepository,
         IProductRepository productRepository,
-        ICompanyRepository company)
+        ICompanyRepository company,
+        IShoppingCartRepository shoppingCart)
     {
         _dbContext = dbContext;
         Category = categoryRepository;
         Product = productRepository;
         Company = company;
+        ShoppingCart = shoppingCart;
     }
 
     public ICategoryRepository CategoryRepository => Category;
 	public IProductRepository ProductRepository => Product;
 	public ICompanyRepository CompanyRepository => Company;
+
+    public IShoppingCartRepository ShoppingCartRepository => ShoppingCart;
 
     public void Save()
 	{
