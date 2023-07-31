2e5d76f1ff55d9cfc35c9af7ba717c181d4a1a52
tgztk <96948081+tgztk@users.noreply.github.com>
2023-07-31 21:34:32 +0300

    Added `ApplicationUserRepository`

---
+++ Bulky.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.Contracts;
+using Bulky.Models.Entities;
+
+namespace Bulky.DataAccess.Repository;
+public class ApplicationUserRepository : Repository<Category>, IApplicationUserRepository
+{
+	private readonly ApplicationDbContext _dbContext;
+
+	public ApplicationUserRepository(ApplicationDbContext dbContext) : base(dbContext)
+	{
+		_dbContext = dbContext;
+	}
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -29,6 +29,7 @@ builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
 builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+builder.Services.AddScoped<IApplicationUserRepository, ApplicationUserRepository>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
