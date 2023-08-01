1cba4550a95adc3ced2e456f8d97a7279e709748
tgztk <96948081+tgztk@users.noreply.github.com>
2023-08-01 21:26:19 +0300

    Added `SessionId` to `OrderHeader` table for each payment intent

--- Bulky.Models/Entities/OrderHeader.cs
+++ Bulky.Models/Entities/OrderHeader.cs
@@ -24,6 +24,7 @@ public class OrderHeader
     public DateTime PaymentDate { get; set; }
     public DateOnly PaymentDueDate { get; set; }
 
+    public string? SessionId { get; set; }
     public string? PaymentIntentId { get; set; }
 
     [Required]
