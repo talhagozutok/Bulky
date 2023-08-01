192ddbcc0db0efaffa3ccbc7cc057b3520d845ac
talhagozutok <talhagozutok@users.noreply.github.com>
2023-08-01 23:58:09 +0300

    Add Helper Methods in Order Header Repository

--- Bulky.DataAccess/Repository/OrderHeaderRepository.cs
+++ Bulky.DataAccess/Repository/OrderHeaderRepository.cs
@@ -16,4 +16,32 @@ public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderReposi
 	{
 		_dbContext.OrderHeaders.Update(orderHeader);
 	}
+	public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+	{
+		var orderFromDb = _dbContext.OrderHeaders.FirstOrDefault(u => u.Id == id);
+
+		if (orderFromDb != null)
+		{
+			orderFromDb.OrderStatus = orderStatus;
+			if (!string.IsNullOrEmpty(paymentStatus))
+			{
+				orderFromDb.PaymentStatus = paymentStatus;
+			}
+		}
+	}
+
+	public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
+	{
+		var orderFromDb = _dbContext.OrderHeaders.FirstOrDefault(u => u.Id == id);
+
+		if (!string.IsNullOrEmpty(sessionId))
+		{
+			orderFromDb.SessionId = sessionId;
+		}
+		if (!string.IsNullOrEmpty(paymentIntentId))
+		{
+			orderFromDb.PaymentIntentId = paymentIntentId;
+			orderFromDb.PaymentDate = DateTime.Now;
+		}
+	}
 }
