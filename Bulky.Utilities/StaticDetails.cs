c681cf39d713ad47ccfc9d3a99df4aff8e4b3fc4
tgztk <96948081+tgztk@users.noreply.github.com>
2023-08-01 15:33:13 +0300

    Order Status

--- Bulky.Utilities/StaticDetails.cs
+++ Bulky.Utilities/StaticDetails.cs
@@ -6,6 +6,18 @@ public static class StaticDetails
     public const string Role_Admin = "Admin";
     public const string Role_Employee = "Employee";
 
+    public const string StatusPending = "Pending";
+    public const string StatusApproved = "Approved";
+    public const string StatusInProcess = "InProcess";
+    public const string StatusShipped = "Shipped";
+    public const string StatusCancelled = "Cancelled";
+    public const string StatusRefunded = "Refunded";
+
+    public const string PaymentStatusPending = "Pending";
+    public const string PaymentStatusApproved = "Approved";
+    public const string PaymentStatusDelayedPayment = "DelayedPayment";
+    public const string PaymentStatusRejected = "Rejected";
+
     public const string AdminEmail = "admin@admin.com";
     public const string AdminPassword = "***REDACTED***";
 }
