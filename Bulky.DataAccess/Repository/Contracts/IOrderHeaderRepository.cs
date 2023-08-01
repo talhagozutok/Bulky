58bcfc3cc3e2663873014b9815bbcbc87fbd717e
tgztk <96948081+tgztk@users.noreply.github.com>
2023-08-01 23:58:09 +0300

    Add Helper Methods in Order Header Repository

--- Bulky.DataAccess/Repository/Contracts/IOrderHeaderRepository.cs
+++ Bulky.DataAccess/Repository/Contracts/IOrderHeaderRepository.cs
@@ -4,4 +4,6 @@ namespace Bulky.DataAccess.Repository.Contracts;
 public interface IOrderHeaderRepository : IRepository<OrderHeader>
 {
 	void Update(OrderHeader orderHeader);
+	void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
+	void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId);
 }
