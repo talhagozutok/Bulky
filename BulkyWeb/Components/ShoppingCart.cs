595f55f701a9e82abd837708f7ac2ec1f8a09d80
talhagozutok <talhagozutok@users.noreply.github.com>
2023-08-03 16:48:42 +0300

    Create View Component

---
+++ BulkyWeb/Components/ShoppingCart.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+using Bulky.DataAccess.Repository.Contracts;
+using Bulky.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Components;
+
+public class ShoppingCart : ViewComponent
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ShoppingCart(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var userId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var session = HttpContext.Session;
+        var sessionCart = StaticDetails.SessionCart;
+
+        if (userId is not null)
+        {
+            if (session.GetInt32(sessionCart) is null)
+            {
+                session.SetInt32(sessionCart, _unitOfWork.ShoppingCarts
+                .GetAll(u => u.ApplicationUserId.Equals(userId))!.Count());
+            }
+
+            return View(session.GetInt32(sessionCart));
+        }
+        else
+        {
+            session.Clear();
+            return View(0);
+        }
+    }
+}
