60087201b1f83eb5fef375bf07df27ea2fcc4698
tgztk <96948081+tgztk@users.noreply.github.com>
2023-08-04 22:54:04 +0300

    Display User List

---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models.Entities;
+using Bulky.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Controllers;
+
+[Area(areaName: "Admin")]
+[Authorize(Roles = StaticDetails.Role_Admin)]
+public class UserController : Controller
+{
+    private readonly ApplicationDbContext _dbContext;
+    public UserController(ApplicationDbContext db)
+    {
+        _dbContext = db;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    #region API CALLS
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        List<ApplicationUser> userList = _dbContext.ApplicationUsers.Include(u => u.Company).ToList();
+
+        return Json(new { data = userList });
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int? id)
+    {
+        return Json(new { success = true, message = "Deleted successfully" });
+    }
+
+    #endregion
+}
---
+++ BulkyWeb/wwwroot/js/user.js
@@ -0,0 +1,34 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#userTable').DataTable({
+        ajax: {
+            url: '/admin/user/getall',
+            type: 'GET'
+        },
+        columns: [
+            { data: 'name', width: '15%' },
+            { data: 'email', width: '15%' },
+            { data: 'phoneNumber', width: '15%' },
+            { data: 'company.name', width: '15%' },
+            { data: '', width: '15%' },
+            {
+                data: 'id',
+                render: function (data) {
+                    return `<div class="btn-group" role="group">
+						        <a href="/admin/user/upsert/${data}" class="btn btn-outline-primary">
+							        <i class="bi bi-pencil-square"></i> Edit
+						        </a>
+					        </div>`
+                },
+                width: '25%',
+                orderable: false,
+                title: 'Operations'
+            }
+        ]
+    });
+}
