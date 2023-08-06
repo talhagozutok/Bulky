fc36ab0ebbb8f0b59909a50a8edbd71ab2ec795f
talhagozutok <talhagozutok@users.noreply.github.com>
2023-08-06 22:56:57 +0300

    Generate QR code for 2FA page

---
+++ BulkyWeb/wwwroot/js/qr.js
@@ -0,0 +1,12 @@
+﻿window.addEventListener("load", () => {
+    const uri = document.getElementById("qrCodeData").getAttribute('data-url');
+    var qrcode = new QRCode(document.getElementById("qrCode"),{
+        text: uri,
+        width: 150,
+        height: 150,
+        padding: 100,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H
+    });
+});
